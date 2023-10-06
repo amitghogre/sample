a87567701fe7365536c4e2563731e696d824daff
amitghogre <144378982+amitghogre@users.noreply.github.com>
2023-10-06 10:34:03 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,45 @@
+'''x=10
+y='amit'
+print(x)
+print('x')
+print(type(x))'''
+''''x=2
+y=4
+c=x+y
+print(c)
+print(x-y)
+print(x**y)
+print(y%x)'''
+# /quot , %(mod or remender) , //(int part)
+'''x=input('enter name')
+print(x)
+print(type(x))
+#y=int(input("age"))'''
+'''x=2
+y=3
+print(x!=y)'''
+'''x=int(input('first no'))
+y=int(input("2nd no"))
+if(x<y):
+    print('wow')
+else:
+    print('oh noo')'''
+"""x=int(input("numb"))
+if(x>0):
+    print("posi")
+elif(x==0):
+    print("nor posi nor negi")
+else:
+    print('negi')"""
+import math
+PI= math.pi
+def calculate_area(radius):
+    area=PI*radius**2
+    return area
+calculate_area(2)
+
+
+
+
+
+
